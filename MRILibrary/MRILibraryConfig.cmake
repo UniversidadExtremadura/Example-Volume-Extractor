8d06f27b8630256db60bed385077a83ea0813274
Juan Pedro <JuanP.95.torres@gmail.com>
2019-09-12 13:48:50 +0200

    Changes to compile example

--- MRILibrary/MRILibraryConfig.cmake
+++ MRILibrary/MRILibraryConfig.cmake
@@ -1,3 +1,3 @@
 add_library(MRILibrary STATIC IMPORTED)
-find_library(MRILibrary_LIBRARY_PATH MRILibrary HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
-set_target_properties(MRILibrary PROPERTIES IMPORTED_LOCATION "${MRILibrary_LIBRARY_PATH}")
+find_library(MRILIBRARY_LIBRARY_PATH /usr/local/lib/ HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
+set_target_properties(MRILibrary PROPERTIES IMPORTED_LOCATION "${MRILIBRARY_LIBRARY_PATH}")
